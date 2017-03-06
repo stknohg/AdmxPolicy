0d226960474aec8b2aa7dbbaa23a16af52f6b257
stknohg <stknohg@gmail.com>
2017-03-06 09:38:28 +0900

    fix for PowerShell 2.0.

--- AdmxPolicy/AddTypes.psm1
+++ AdmxPolicy/AddTypes.psm1
@@ -3,4 +3,10 @@ Set-Strictmode -Version 2.0
 # load AdmxPolicy classes
 # In PowerShell 2.0, this file must be loaded first.
 $RootPath = Split-path $script:MyInvocation.MyCommand.Path -Parent
-Add-Type -Path (Join-Path $RootPath "AdmxPolicy.cs")
+if ( $PSVersionTable.PSVersion.Major -eq 2.0 ) {
+    # PowerShell 2.0 dosen't have System.Management.Automation.HiddenAttribute .
+    $source = ((Get-Content (Join-Path $RootPath "AdmxPolicy.cs") -Encoding UTF8) -join "`r`n").Replace('[System.Management.Automation.HiddenAttribute]','')
+    Add-Type -TypeDefinition $source
+} else {
+    Add-Type -Path (Join-Path $RootPath "AdmxPolicy.cs")
+}
