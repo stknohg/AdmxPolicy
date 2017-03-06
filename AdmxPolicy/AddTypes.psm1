6527c288c8ac06ac63b3a796c10b49097dcdbd27
stknohg <stknohg@gmail.com>
2017-03-06 16:37:27 +0900

    fix usage of System.Management.Automation.HiddenAttribute .

--- AdmxPolicy/AddTypes.psm1
+++ AdmxPolicy/AddTypes.psm1
@@ -3,10 +3,9 @@ Set-Strictmode -Version 2.0
 # load AdmxPolicy classes
 # In PowerShell 2.0, this file must be loaded first.
 $RootPath = Split-path $script:MyInvocation.MyCommand.Path -Parent
-if ( $PSVersionTable.PSVersion.Major -eq 2.0 ) {
-    # PowerShell 2.0 dosen't have System.Management.Automation.HiddenAttribute .
-    $source = ((Get-Content (Join-Path $RootPath "AdmxPolicy.cs") -Encoding UTF8) -join "`r`n").Replace('[System.Management.Automation.HiddenAttribute]','')
-    Add-Type -TypeDefinition $source
+if ( $PSVersionTable.PSVersion.Major -lt 5.0 ) {
+    # PowerShell 2.0 - 4.0 dosen't have System.Management.Automation.HiddenAttribute .
+    Add-Type -TypeDefinition ((Get-Content (Join-Path $RootPath "AdmxPolicy.cs") -Encoding UTF8) -join "`r`n").Replace('[System.Management.Automation.HiddenAttribute]','')
 } else {
     Add-Type -Path (Join-Path $RootPath "AdmxPolicy.cs")
 }
