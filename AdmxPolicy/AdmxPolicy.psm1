6772beb333260ca7c822cadd6f407a105ae53a84
stknohg <stknohg@gmail.com>
2017-03-08 13:52:14 +0900

    fix ValueDefinition.Value types.

--- AdmxPolicy/AdmxPolicy.psm1
+++ AdmxPolicy/AdmxPolicy.psm1
@@ -195,11 +195,11 @@ function GetValueDefinitionFromXmlNode ([Xml.XmlElement]$ValueElement) {
         }
         "decimal" {
             $type = [AdmxPolicy.ValueTypes]::Decimal
-            $value = [decimal]$ValueElement.FirstChild.value
+            $value = [Int32]$ValueElement.FirstChild.value
         }
         "longdecimal" {
             $type = [AdmxPolicy.ValueTypes]::LongDecimal
-            $value = [decimal]$ValueElement.FirstChild.value
+            $value = [Int64]$ValueElement.FirstChild.value
         }
         "string" {
             $type = [AdmxPolicy.ValueTypes]::String
